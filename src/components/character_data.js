334940245339a8fd17d21c8949584c11f9046dd6
SVigramR <sabareeshvigram654@gmail.com>
2024-05-19 13:58:05 +0530

    Added character images to the cards

--- src/components/character_data.js
+++ src/components/character_data.js
@@ -1,14 +1,29 @@
+import anakin_skywalker from '../images/anakin_skywalker.png';
+import dass_jennir from '../images/dass_jennir.png';
+import leska from '../images/leska.png';
+import shaak_ti from '../images/shaak_ti.png';
+import luke_skywalker from '../images/luke_skywalker.png';
+import quinlan_vos from '../images/quinlan_vos.png';
+import obiwan_kenobi from '../images/Obiwan_Kenobi.png';
+import rahm_kota from '../images/Rahm_Kota.png';
+import yoda from '../images/yoda.png';
+import mace_windu from '../images/mace_windu.png';
+import kai_jutiss from '../images/kai_justiss.png';
+import etain_tur_mukan from '../images/etain_tur_mukan.png';
+
 const characterData = [
-    { image: '', name: 'Anakin Skywalker', selected: false },
-    { image: '', name: 'Obi-Wan Kenobi', selected: false },
-    { image: '', name: 'Mace Windu', selected: false },
-    { image: '', name: 'Yoda', selected: false },
-    { image: '', name: 'Luke Skywalker', selected: false },
-    { image: '', name: 'Etain Tur-Mukan', selected: false },
-    { image: '', name: 'Kai Justiss', selected: false },
-    { image: '', name: 'Rahm Kota', selected: false },
-    { image: '', name: 'Quinlan Vos', selected: false },
-    { image: '', name: 'Dass Jennir', selected: false },
+    { image: anakin_skywalker, name: 'Anakin Skywalker', selected: false },
+    { image: obiwan_kenobi, name: 'Obi-Wan Kenobi', selected: false },
+    { image: mace_windu, name: 'Mace Windu', selected: false },
+    { image: yoda, name: 'Yoda', selected: false },
+    { image: luke_skywalker, name: 'Luke Skywalker', selected: false },
+    { image: etain_tur_mukan, name: 'Etain Tur-Mukan', selected: false },
+    { image: kai_jutiss, name: 'Kai Justiss', selected: false },
+    { image: rahm_kota, name: 'Rahm Kota', selected: false },
+    { image: quinlan_vos, name: 'Quinlan Vos', selected: false },
+    { image: dass_jennir, name: 'Dass Jennir', selected: false },
+    { image: shaak_ti, name: 'Shak Ti', selected: false },
+    { image: leska, name: 'Leska', selected: false },
   ];
 
 export default characterData;
