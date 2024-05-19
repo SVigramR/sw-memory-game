33f5dbcf730fcb2bd5378fce7b4c05159f2d5e37
SVigramR <sabareeshvigram654@gmail.com>
2024-05-19 15:35:19 +0530

    Styled header section of the game

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
+import '../styles/header.css';
+
 function HeaderSection({ currentScore, bestScore }) {
     return (
         <>
+        <div className="headerSection">
             <h1>Star Wars Memory Game</h1>
-            <div className="scoreBord">
+            <div className="scoreBoard">
                 <h3 className="currentScore">Score: {currentScore}</h3>
                 <h3 className="bestScore">Best Score: {bestScore}</h3>
             </div>
+        </div>
         </>
     )
 }
