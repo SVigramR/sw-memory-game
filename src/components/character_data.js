ed6431218252f33d7d04ab80ae1d4a8af6b23d39
SVigramR <sabareeshvigram654@gmail.com>
2024-05-19 15:36:04 +0530

    Restructured the image file structure for deployment in vercel

--- src/components/character_data.js
+++ src/components/character_data.js
@@ -1,29 +1,16 @@
-import anakin_skywalker from '../images/anakin_skywalker.png';
-import dass_jennir from '../images/dass_jennir.png';
-import leska from '../images/leska.png';
-import shaak_ti from '../images/shaak_ti.png';
-import luke_skywalker from '../images/luke_skywalker.png';
-import quinlan_vos from '../images/quinlan_vos.png';
-import obiwan_kenobi from '../images/Obiwan_Kenobi.png';
-import rahm_kota from '../images/Rahm_Kota.png';
-import yoda from '../images/yoda.png';
-import mace_windu from '../images/mace_windu.png';
-import kai_jutiss from '../images/kai_justiss.png';
-import etain_tur_mukan from '../images/etain_tur_mukan.png';
-
 const characterData = [
-    { image: anakin_skywalker, name: 'Anakin Skywalker', selected: false },
-    { image: obiwan_kenobi, name: 'Obi-Wan Kenobi', selected: false },
-    { image: mace_windu, name: 'Mace Windu', selected: false },
-    { image: yoda, name: 'Yoda', selected: false },
-    { image: luke_skywalker, name: 'Luke Skywalker', selected: false },
-    { image: etain_tur_mukan, name: 'Etain Tur-Mukan', selected: false },
-    { image: kai_jutiss, name: 'Kai Justiss', selected: false },
-    { image: rahm_kota, name: 'Rahm Kota', selected: false },
-    { image: quinlan_vos, name: 'Quinlan Vos', selected: false },
-    { image: dass_jennir, name: 'Dass Jennir', selected: false },
-    { image: shaak_ti, name: 'Shak Ti', selected: false },
-    { image: leska, name: 'Leska', selected: false },
+    { image: '/static/images/anakin_skywalker.png', name: 'Anakin Skywalker', selected: false },
+    { image: '/static/images/Obiwan_Kenobi.png', name: 'Obi-Wan Kenobi', selected: false },
+    { image: '/static/images/mace_windu.png', name: 'Mace Windu', selected: false },
+    { image: '/static/images/yoda.png', name: 'Yoda', selected: false },
+    { image: '/static/images/luke_skywalker.png', name: 'Luke Skywalker', selected: false },
+    { image: '/static/images/etain_tur_mukan.png', name: 'Etain Tur-Mukan', selected: false },
+    { image: '/static/images/kai_justiss.png', name: 'Kai Justiss', selected: false },
+    { image: 'public/static/images/Rahm_Kota.png', name: 'Rahm Kota', selected: false },
+    { image: '/static/images/quinlan_vos.png', name: 'Quinlan Vos', selected: false },
+    { image: '/static/images/dass_jennir.png', name: 'Dass Jennir', selected: false },
+    { image: '/static/images/shaak_ti.png', name: 'Shaak Ti', selected: false },
+    { image: '/static/images/leska.png', name: 'Leska', selected: false },
   ];
 
 export default characterData;
